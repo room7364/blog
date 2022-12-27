599a5a8759ad3c0aaf118d1d7d0a4bbd1cfa9309
Oleg Startsev <room3764@gmail.com>
2022-12-27 19:49:07 +0500

    Try using resourse route and link_to function

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   #! Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # This route declares that "GET /articles" requests are mapped to the "index" action of "ArticlesController"
-  get "/articles", to: "articles#index"
+  #get "/articles", to: "articles#index"
+  
   # Add a route that maps the root path of our application to the appropriate controller and action
   root "articles#index"
+
   # ":id" designates a route parameter. A route parameter captures a segment of the request's path, and puts that value into the "params" Hash, which is accessible by the controller action.
-  get "/articles/:id", to: "articles#show"
+  #get "/articles/:id", to: "articles#show"
+  
+  # Rails provides a routes method names "resources" that maps all of the conventional routes for a collection of resources, such as articles.
+  resources :articles
 end
